52a7725718e8baad7d0a48458f4903cb48d41051
clee2691 <clee2691@gmail.com>
2015-12-20 22:38:14 -0500

    Made the functions more universal. Created class instances with a loop. Created grocery item instances. Added comments

--- GroceryCompare/GroceryCompare.py
+++ GroceryCompare/GroceryCompare.py
@@ -1,43 +1,46 @@
 ﻿from bs4 import BeautifulSoup, UnicodeDammit
 import requests
 
-class groceryItem():
+class GroceryItem():
+    
+    # Creates grocery item instance
     def __init__(self, item_name, item_price, item_image):
         self.item_name = item_name
         self.item_price = item_price
         self.item_image = item_image
 
-def parsePage():
-    acmePage = requests.get('http://acmemarkets.mywebgrocer.com/Circular/Philadelphia-19th-and-Oregon/744373054/Weekly/3')
-    acmeSource = BeautifulSoup(acmePage.content, 'html.parser')
-    return acmeSource
+def parsePage(web_page):
+
+    #Parses webpage of requested ad page
+    page = requests.get(web_page)
+    return BeautifulSoup(page.content, 'html.parser')
 
 def findGroceryDetails(grocery_store):
 
     #selects the css selector and puts them in a list
-    all_p_prices = grocery_store.select('.itemPrice')
-    all_p_titles = grocery_store.select('.itemTitle')
-    all_p_images = grocery_store.select('.itemImage')
+    prices = grocery_store.select('.itemPrice')
+    titles = grocery_store.select('.itemTitle')
+    images = grocery_store.select('.itemImage')
 
-    return all_p_prices, all_p_titles, all_p_images
+    return prices, titles, images
 
-def createGroceryItem():
-    acme_price, acme_names, acme_images = findGroceryDetails(acmeSource)
+def createGroceryItem(page_source):
+    
+    # Returns a list of all the names, prices, and images for each item on the ad page
+    price, names, images = findGroceryDetails(page_source)
 
-    for p, n, i in zip(acme_price, acme_names, acme_images):
-        print ('Item: '.encode() + n.get_text().encode(), 'Price: '.encode() + p.get_text().encode(), 'Image: '.encode() + i.get_text().encode())
+    # Create instances of the grocery item using GroceryItem class
+    item_list = []
+    for p, n, i in zip(price, names, images):
+        item_list.append(GroceryItem(n.get_text().encode(),p.get_text().encode(),i.get('src').encode()))
+
+    # Debug info. Shows item name and price so far
+    for item in item_list:
+        print(item.item_name)
+        print(item.item_price)
+
 
 if __name__ == '__main__':
 
-    acmeSource = parsePage()
-    acmeGroceries = createGroceryItem()
-
-    # for debugging purposes
-    '''for prices in acme_price:
-        print(prices.encode('UTF-8'))
-
-    for titles in acme_names:
-        print(titles.encode('UTF-8'))
-
-    for images in acme_images:
-        print(images.encode('UTF-8'))'''
+    acme_source = parsePage('http://acmemarkets.mywebgrocer.com/Circular/Philadelphia-19th-and-Oregon/744373054/Weekly/3')
+    acmeGroceries = createGroceryItem(acme_source)
