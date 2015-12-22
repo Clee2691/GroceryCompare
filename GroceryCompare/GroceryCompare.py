ca53b57f063fdc4a93bed27386aa8efa64cb4225
clee2691 <clee2691@gmail.com>
2015-12-21 23:06:24 -0500

    Added search functionality. Added shoprite store.

--- GroceryCompare/GroceryCompare.py
+++ GroceryCompare/GroceryCompare.py
@@ -34,13 +34,54 @@ def createGroceryItem(page_source):
     for p, n, i in zip(price, names, images):
         item_list.append(GroceryItem(n.get_text().encode(),p.get_text().encode(),i.get('src').encode()))
 
-    # Debug info. Shows item name and price so far
-    for item in item_list:
-        print(item.item_name)
-        print(item.item_price)
+    return item_list
 
+def searchItem(item_list):
+    search_item = input('What is the name of the item to search for?: ')
+    item_found_list = []
+
+    for item in item_list:
+        if item.item_name.find(search_item.encode()) > -1:
+            item_found_list.append(item)
+
+    return item_found_list
+
+def display_items_found(found_list):
+    for item in found_list:
+        print('Item name: '.encode() + item.item_name, 'Price: '.encode() + item.item_price)
 
 if __name__ == '__main__':
 
-    acme_source = parsePage('http://acmemarkets.mywebgrocer.com/Circular/Philadelphia-19th-and-Oregon/744373054/Weekly/3')
-    acmeGroceries = createGroceryItem(acme_source)
+    acme_source = parsePage('http://acmemarkets.mywebgrocer.com/Circular/Philadelphia-19th-and-Oregon/744373054/Weekly/3/1')
+    shoprite_source = parsePage('http://plan.shoprite.com/Circular/ShopRite-of-Oregon-Ave/977B652/Weekly/2/1')
+    acme_groceries = createGroceryItem(acme_source)
+    shoprite_groceries = createGroceryItem(shoprite_source)
+
+    while True:
+        search_for_groceries = input('Do you want to search for specific items? (Y/N/Exit): ')
+     
+        # Affirmative to search for items
+        if search_for_groceries == 'Y' or search_for_groceries == 'y':
+            found_item_list = searchItem(acme_groceries)
+
+            #Only print the items if things have been found
+            if len(found_item_list) > 0:
+                display_items_found(found_item_list)
+            else:
+                print('Sorry no items found this week. Try a different item.')
+        
+        #Print all items on the page
+        elif search_for_groceries == 'N' or search_for_groceries == 'n':
+            print('\nHere are this weeks groceries: \n' + 'ACME Store:\n')
+            for item in acme_groceries:
+                print('Item name: '.encode() + item.item_name, 'Price: '.encode() + item.item_price)
+            
+            print('\nHere are this weeks groceries: \n' + 'ShopRite Store\n')
+            for item in shoprite_groceries:
+                print('Item name: '.encode() + item.item_name, 'Price: '.encode() + item.item_price)
+        #Exit the app
+        elif search_for_groceries == 'exit' or search_for_groceries == 'Exit':
+            break
+
+        else:
+            print('Please put Y or N')
