b970d12ae05000ba89ce1f83353f0b47e0b1042d
clee2691 <clee2691@gmail.com>
2015-12-28 21:25:46 -0500

    Fixed page parser to parse all pages. Returns a list of all the items from store

--- GroceryCompare/GroceryCompare.py
+++ GroceryCompare/GroceryCompare.py
@@ -15,7 +15,7 @@ class GroceryLayout(QtWidgets.QMainWindow, Ui_MainWindow):
 
 class GroceryItem():
     
-    # Creates grocery item instance
+    '''Creates the item instances found on each page'''
     def __init__(self, item_name, item_price, item_image):
         self.item_name = item_name
         self.item_price = item_price
@@ -25,15 +25,18 @@ def parsePages(web_page):
 
     ''' Takes generic page and makes a working link of the ad for further processing''' 
 
+    #Generic store link souped
     store_link_source = requests.get(web_page)
     store_link_Souped = BeautifulSoup(store_link_source.content, 'html.parser')
 
+    #Find the actual link to the circular
     week_ad_link = store_link_Souped.select('.megadrop-circular-name')
     week_ad_link = week_ad_link[0].get('href')
 
+    #Create the full link without pages
     full_link = web_page + str(week_ad_link)
-    print(full_link)
 
+    #Send link to make the dictionary of grocery pages
     page = requests.get(full_link)
     shop_dic = makeGroceryDictionary(BeautifulSoup(page.content, 'html.parser'), full_link)
 
@@ -44,23 +47,23 @@ def makeGroceryDictionary(store_page_source, actual_page):
 
     grocery_page_dic = {}
 
+    #Determine how many pages are in this weeks ads
     all_pages_tag = store_page_source.select('.pages')
+  
+    num_pages = int((len(all_pages_tag) + 2) / 2) # Use "2" to correct for double "pages" effectively making the total pages twice as many
 
-
-    num_pages = int((len(all_pages_tag) + 2) / 2)
-
+    #Parses each ad page and makes them into BS objects
     x = 1
 
     for i in range(1, num_pages + 1):
         page_iter = requests.get(actual_page + '/' + str(i))
 
-        grocery_page_dic['page'+ str(x)] = BeautifulSoup(page_iter.content, 'html.parser')
+        grocery_page_dic[str(x)] = BeautifulSoup(page_iter.content, 'html.parser')
 
         x += 1
 
     return grocery_page_dic
     
-
 def findGroceryDetails(grocery_store):
 
     '''selects the css selector for the item details and puts them in a list'''
@@ -71,15 +74,16 @@ def findGroceryDetails(grocery_store):
 
     return prices, titles, images
 
-def createGroceryItem(page_source):
-    
-    # Returns a list of all the names, prices, and images for each item on the ad page
-    price, names, images = findGroceryDetails(page_source)
-
-    # Create instances of the grocery item using GroceryItem class
+def createGroceryItem(page_dic):
     item_list = []
-    for p, n, i in zip(price, names, images):
-        item_list.append(GroceryItem(n.get_text().encode(),p.get_text().encode(),i.get('src').encode()))
+
+    for k,v in page_dic.items():
+        # Returns a list of all the names, prices, and images for each item on the ad page
+        price, names, images = findGroceryDetails(v)
+
+        # Create instances of the grocery item using GroceryItem class
+        for p, n, i in zip(price, names, images):
+            item_list.append(GroceryItem(n.get_text().encode(),p.get_text().encode(),i.get('src').encode()))
 
     return item_list
 
@@ -100,13 +104,13 @@ def display_items_found(found_list):
 
 if __name__ == '__main__':
 
-    #Make this loop over all pages of the ad. Try using a dictionary
-    acme_source = parsePages('http://acmemarkets.mywebgrocer.com')
-    shoprite_source = parsePages('http://plan.shoprite.com')
+    #Parses each of the pages of the ad for each store. Returns a dictionary of the pages
+    acme_dic = parsePages('http://acmemarkets.mywebgrocer.com')
+    #shoprite_dic = parsePages('http://plan.shoprite.com')
 
 
-    acme_groceries = createGroceryItem(acme_source)
-    shoprite_groceries = createGroceryItem(shoprite_source)
+    acme_groceries = createGroceryItem(acme_dic)
+    #shoprite_groceries = createGroceryItem(shoprite_dic)
 
     #Ensures it works in virtualEnv by setting paths
     QtCore.QCoreApplication.setLibraryPaths(['E:\Visual Studio 2015\Projects\GroceryCompare\GroceryCompare\env34\Lib\site-packages\PyQt5\plugins'])
